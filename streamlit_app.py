02a46dfa0ce19c63635b29355c56a3d79885aaa3
nracharla2002 <121835285+nracharla2002@users.noreply.github.com>
2023-01-02 21:55:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text()
 streamlit.text('Omega 3 & Blueberry Oatmeal')   
 streamlit.text('Kale,Spinach & Rocket smoothie')              
